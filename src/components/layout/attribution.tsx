633b6594bb31ab4ff869e866b4acda58de8dac4a
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-27 17:07:38 +0300

    refactor: update attribution component to remove Frontend Mentor reference

--- src/components/layout/attribution.tsx
+++ src/components/layout/attribution.tsx
@@ -1,16 +1,7 @@
 export default function Attribution() {
   return (
     <p className="v-page pb-0.5 text-center text-xs text-white/60">
-      Challenge by{" "}
-      <a
-        href="https://www.frontendmentor.io?ref=challenge"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline underline-offset-2 hover:text-white"
-      >
-        Frontend Mentor
-      </a>
-      . Coded by{" "}
+      Coded by{" "}
       <a
         href="https://www.linkedin.com/in/abdelrhman-vanta/"
         target="_blank"
